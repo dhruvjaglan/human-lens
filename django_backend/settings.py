f08517bcc4c6bc055f2a091369a3c5658544e20a
dhruvjaglan <dhruvjaglan2@gmail.com>
2024-06-23 15:27:47 +0530

    redis setting updated

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -114,8 +114,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('localhost', 6379)],
-            #  "hosts": [('redis', 6379)],
+            # 'hosts': [('localhost', 6379)],
+              "hosts": [('redis', 6379)],
         },
     },
 }
